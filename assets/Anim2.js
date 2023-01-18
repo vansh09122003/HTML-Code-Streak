c0fad2cd043d6e6a8bacdca7283ca7733363b176
Tanishq9TSJ <jadhavtanishq09@gmail.com>
2023-01-19 02:25:54 +0530

    Anim2 and quotes

--- assets/Anim2.js
+++ assets/Anim2.js
@@ -8,16 +8,24 @@ class Anim2 extends Phaser.Scene{
         this.load.image('bk','assets/Background_2.png');
         this.load.image('yellow','assets/yellow.jpg');
         this.load.image('red','assets/red.jpg');
+        this.load.image('blue','assets/blue.jpg');
+        this.load.image('green','assets/green.jpg');
     }
 
     create(){
 
         this.add.image(500,400,'bk');
-        this.yellow=this.physics.add.image(300,0,'yellow').setScale(0.3); 
-        this.yellow.setVelocityY(200);
+        this.yellow=this.physics.add.image(400,0,'yellow').setScale(0.3); 
+        this.yellow.setVelocityY(50);
 
-        this.red=this.physics.add.image(1075,800,'red').setScale(0.3); 
-        this.red.setVelocityY(-200);
+        this.red=this.physics.add.image(1025,800,'red').setScale(0.3); 
+        this.red.setVelocityY(-50);
+
+        this.blue=this.physics.add.image(10,10,'blue').setScale(0.3);
+        this.blue.setVelocityX(50); 
+
+        this.green=this.physics.add.image(1496,564,'green').setScale(0.3);
+        this.green.setVelocityX(-50); 
     }
 
    
